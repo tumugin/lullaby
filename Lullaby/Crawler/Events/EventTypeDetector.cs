750cce1dccc566ca8f0ef01f3e045855c5b40b16
Kazuki Oishi <oishikazuki@gmail.com>
2022-11-26 03:12:36 +0900

    add tests

--- Lullaby/Crawler/Events/EventTypeDetector.cs
+++ Lullaby/Crawler/Events/EventTypeDetector.cs
@@ -4,7 +4,7 @@ public class EventTypeDetector
 {
 	private readonly string[] _battleEventNames =
 	{
-		"HYPE", "Funpal", "MAWA LOOP", "SPARK", "アイドルアラモード", "MARQUEE祭", "dot yell"
+		"HYPE", "Funpal", "MAWA LOOP", "アイドルアラモード", "MARQUEE祭", "dot yell", "TOKYO GIRLS GIRLS"
 	};
 
 	private readonly string[] _fesEventNames =
@@ -16,15 +16,17 @@ public class EventTypeDetector
 		"エンドレスサマー",
 		"@JAM",
 		"SEKIGAHARA IDOL WARS",
-		"関ケ原唄姫合戦"
+		"関ケ原唄姫合戦",
+		"SPARK",
+		"IDORISE"
 	};
 
 	public EventType DetectEventTypeByTitle(string eventTitle) =>
 		eventTitle switch
 		{
-			not null when eventTitle.Contains("対バン") => EventType.BATTLE,
 			not null when _battleEventNames.Any(eventTitle.Contains) => EventType.BATTLE,
 			not null when _fesEventNames.Any(eventTitle.Contains) => EventType.FES,
+			not null when eventTitle.Contains("対バン") => EventType.BATTLE,
 			_ => EventType.NOT_DEFINED
 		};
 }
