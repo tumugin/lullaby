76608bab10ec76c931f2a9c0022925c0be857575
Kazuki Oishi <oishikazuki@gmail.com>
2022-11-27 19:04:58 +0900

    fix type check

--- Lullaby/Services/Event/AddEventByGroupEventService.cs
+++ Lullaby/Services/Event/AddEventByGroupEventService.cs
@@ -12,16 +12,18 @@ public class AddEventByGroupEventService
 
     public async Task<Event> execute(string groupKey, GroupEvent groupEvent)
     {
-        var eventStarts = groupEvent.EventDateTime is DetailedEventDateTime
-            ? ((DetailedEventDateTime)groupEvent.EventDateTime).EventStartDateTime
-            : groupEvent.EventDateTime is UnDetailedEventDateTime
-                ? ((UnDetailedEventDateTime)groupEvent.EventDateTime).EventStartDate
-                : throw new ArgumentException("EventDateTime is not a valid type");
-        var eventEnds = groupEvent.EventDateTime is DetailedEventDateTime
-            ? ((DetailedEventDateTime)groupEvent.EventDateTime).EventEndDateTime
-            : groupEvent.EventDateTime is UnDetailedEventDateTime
-                ? ((UnDetailedEventDateTime)groupEvent.EventDateTime).EventEndDate
-                : throw new ArgumentException("EventDateTime is not a valid type");
+        var eventStarts = groupEvent.EventDateTime switch
+        {
+            DetailedEventDateTime detailedEventDateTime => detailedEventDateTime.EventStartDateTime,
+            UnDetailedEventDateTime unDetailedEventDateTime => unDetailedEventDateTime.EventStartDate,
+            _ => throw new ArgumentException("EventDateTime is not a valid type"),
+        };
+        var eventEnds = groupEvent.EventDateTime switch
+        {
+            DetailedEventDateTime detailedEventDateTime => detailedEventDateTime.EventEndDateTime,
+            UnDetailedEventDateTime unDetailedEventDateTime => unDetailedEventDateTime.EventEndDate,
+            _ => throw new ArgumentException("EventDateTime is not a valid type"),
+        };
 
         var draftEvent = new Event()
         {
