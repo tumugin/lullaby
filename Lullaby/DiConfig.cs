0ebcfe03ccc6c83a81fc6d0859f2ae835a4b2351
Kazuki Oishi <oishikazuki@gmail.com>
2022-12-05 04:13:12 +0900

    Add DI

---
+++ Lullaby/DiConfig.cs
@@ -0,0 +1,11 @@
+﻿namespace Lullaby;
+
+using Services.Event;
+
+public static class DiConfig
+{
+    public static void BuildDi(WebApplicationBuilder builder)
+    {
+        builder.Services.AddScoped<GetEventsByGroupKeyService, GetEventsByGroupKeyService>();
+    }
+}
