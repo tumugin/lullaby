662650fe0566aaaa0d6f976f5fbf0256a6af7bda
Kazuki Oishi <oishikazuki@gmail.com>
2022-12-10 17:58:47 +0900

    add tests

--- Lullaby.Tests/Crawler/Groups/BaseGroupTest.cs
+++ Lullaby.Tests/Crawler/Groups/BaseGroupTest.cs
@@ -1,9 +1,33 @@
 namespace Lullaby.Tests.Crawler.Groups;
 
+using Lullaby.Services.Event;
+using Microsoft.EntityFrameworkCore;
+
 public class BaseGroupTest : BaseDatabaseTest
 {
-    [Test]
-    public async Task TestGetAndUpdateSavedEvents()
+    private AddEventByGroupEventService AddEventByGroupEventService { get; set; } = null!;
+    private FindDuplicateEventService FindDuplicateEventService { get; set; } = null!;
+    private UpdateEventByGroupEventService UpdateEventByGroupEventService { get; set; } = null!;
+
+    [SetUp]
+    public void Setup()
     {
+        this.AddEventByGroupEventService = new AddEventByGroupEventService(this.Context);
+        this.FindDuplicateEventService = new FindDuplicateEventService(this.Context);
+        this.UpdateEventByGroupEventService = new UpdateEventByGroupEventService(this.Context);
+    }
+
+    [Test]
+    public async Task TestGetAndUpdateSavedEventsWithEmptyDatabase()
+    {
+        var testGroup = new TestGroup();
+        await testGroup.GetAndUpdateSavedEvents(
+            this.AddEventByGroupEventService,
+            this.FindDuplicateEventService,
+            this.UpdateEventByGroupEventService
+        );
+
+        var addedResult = await this.Context.Events.ToListAsync();
+        Assert.That(addedResult, Has.Count.EqualTo(2));
     }
 }
