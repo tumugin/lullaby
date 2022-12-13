888c4ef7eb50a772d37bf466f14c88786c0de4c4
Kazuki Oishi <oishikazuki@gmail.com>
2022-12-13 17:31:12 +0900

    add DI

--- Lullaby/DiConfig.cs
+++ Lullaby/DiConfig.cs
@@ -7,5 +7,8 @@ public static class DiConfig
     public static void BuildDi(WebApplicationBuilder builder)
     {
         builder.Services.AddScoped<GetEventsByGroupKeyService, GetEventsByGroupKeyService>();
+        builder.Services.AddScoped<AddEventByGroupEventService, AddEventByGroupEventService>();
+        builder.Services.AddScoped<FindDuplicateEventService, FindDuplicateEventService>();
+        builder.Services.AddScoped<UpdateEventByGroupEventService, UpdateEventByGroupEventService>();
     }
 }
