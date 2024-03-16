f8812b2d2b4e7d7a6eefb39d04ac024b6dfa0152
Kazuki Oishi <oishikazuki@gmail.com>
2024-03-17 05:41:32 +0900

    Added probe controller for /healthz

--- Lullaby.Admin/Controllers/ErrorController.cs
+++ Lullaby.Admin/Controllers/ErrorController.cs
@@ -1,16 +1,27 @@
 namespace Lullaby.Admin.Controllers;
 
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 
+[Route("[controller]")]
 public class ErrorController : Controller
 {
     [AllowAnonymous]
-    [Route("/Error/{errorCode}")]
-    public IActionResult Error(int errorCode) =>
-        errorCode switch
+    [Route("{errorCode}")]
+    public IActionResult Error(int errorCode)
+    {
+        var statusCodeReExecuteFeature = this.HttpContext.Features.Get<IStatusCodeReExecuteFeature>();
+
+        if (statusCodeReExecuteFeature == null)
+        {
+            return this.NotFound();
+        }
+
+        return errorCode switch
         {
             404 => this.View("404"),
             _ => this.View(errorCode)
         };
+    }
 }
