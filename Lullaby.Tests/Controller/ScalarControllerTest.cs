7a285e11784700bdff1a04c2e0e7a9188caede77
Kazuki Oishi <oishikazuki@gmail.com>
2025-11-17 01:02:12 +0900

    chore: rename SwaggerControllerTest to ScalarControllerTest and update endpoint tests

--- Lullaby.Tests/Controller/ScalarControllerTest.cs
+++ Lullaby.Tests/Controller/ScalarControllerTest.cs
@@ -2,19 +2,19 @@ namespace Lullaby.Tests.Controller;
 
 using System.Net;
 
-public class SwaggerControllerTest : BaseWebTest
+public class ScalarControllerTest : BaseWebTest
 {
     [Test]
     public async Task TestSwaggerEndpoint()
     {
-        using var result = await this.Client.GetAsync("/swagger");
+        using var result = await this.Client.GetAsync("/scalar");
         Assert.That(result.StatusCode, Is.EqualTo(HttpStatusCode.OK));
     }
 
     [Test]
     public async Task TestSwaggerApiEndpoint()
     {
-        using var result = await this.Client.GetAsync("/swagger/v1/swagger.json");
+        using var result = await this.Client.GetAsync("/openapi/v1.json");
         Assert.That(result.StatusCode, Is.EqualTo(HttpStatusCode.OK));
     }
 }
